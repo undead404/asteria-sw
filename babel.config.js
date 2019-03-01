834bfcd729b651c8fbb295a24b90747ce23004c2
Vitalii Perehonchuk <brute18@gmail.com>
2019-03-01 17:57:15 +0200

    removed unnecessary credits

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,15 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 // Babel configuration
 // https://babeljs.io/docs/usage/api/
 module.exports = {
   ignore: ['node_modules', 'build'],
   plugins: [
     // Stage 2
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
     '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-throw-expressions',
@@ -21,7 +17,12 @@ module.exports = {
     // Stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: false,
+      },
+    ],
     '@babel/plugin-proposal-json-strings',
   ],
   presets: ['@babel/preset-env', '@babel/preset-react'],
